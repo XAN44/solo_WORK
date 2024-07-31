64ea3bf936c340b28cca2d3468d1c31ec20e7bb7
XAN44 <nattapanlateh26@gmail.com>
2024-07-31 12:04:57 +0700

    Update page.tsx

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -15,8 +15,32 @@ import { useQuery } from "@tanstack/react-query";
 
 async function Page() {
   const user = await currentUser();
+  const data = await getProfileTeamById(user?.id || "");
+  const userTeamMembership = await FetchTeam();
+  const team = await getTeams();
 
-  return <div className="h-full w-full">{user?.username}</div>;
+  return (
+    <div className="h-full w-full">
+      <div className="flex flex-col space-y-3 md:flex-row md:space-x-3 md:space-y-0">
+        <LevelGate allowedLevel={UserLevel.Admin}>
+          <Admin />
+        </LevelGate>
+        {userTeamMembership ? <Attendence /> : null}
+        <LevelGate allowedLevel={UserLevel.Admin}>
+          <Attendence />
+        </LevelGate>
+        {data?.id ? (
+          <>
+            <YourProfile id={data?.id || ""} />
+          </>
+        ) : null}
+
+        <LevelGate allowedLevel={UserLevel.Admin}>
+          <ConfigSalary />
+        </LevelGate>
+      </div>
+    </div>
+  );
 }
 
 export default Page;
