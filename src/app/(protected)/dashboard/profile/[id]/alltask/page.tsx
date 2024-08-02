d1ef35325fea3a0002f2ac6492f05138d41772bd
XAN44 <nattapanlateh26@gmail.com>
2024-08-02 10:18:49 +0700

    Update page.tsx

--- src/app/(protected)/dashboard/profile/[id]/alltask/page.tsx
+++ src/app/(protected)/dashboard/profile/[id]/alltask/page.tsx
@@ -1,26 +1,32 @@
+"use client";
 import React from "react";
 
 import SumaryTask from "../../../../../../components/ui/allTask/sumaryTask";
 import { useQuery } from "@tanstack/react-query";
 import { fetchTasks } from "../../../../../../../actionAPi/fetch";
 import { ClipLoader } from "react-spinners";
-import {
-  FetchAllTaskForSumery,
-  FetchTask,
-} from "../../../../../../../data/fetch-task";
-import { FetchAllTaskAndAtten } from "../../../../../../../data/fetch-taskAndAtten";
-import { FetchDataSumary } from "../../../../../../../data/fetch-Salary";
 
-export default async function Page({ params }: { params: { id: string } }) {
-  const data = await FetchAllTaskAndAtten(params.id);
-  const amount = await FetchDataSumary(params.id);
-  if (!data) {
+export default function Page({ params }: { params: { id: string } }) {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["AllTask", params.id],
+    queryFn: () => fetchTasks(params.id),
+  });
+
+  if (isLoading) {
+    return (
+      <div className="flex h-full w-full items-center justify-center">
+        <ClipLoader />
+      </div>
+    );
+  }
+  if (error || !data) {
     return <div>No data found.</div>;
   }
 
   const amout = {
-    totalAmount: amount.totalAmount, // Ensure this is correct
-    tasks: data.task || [],
+    totalAmount: data.totalAmount || 0, // Ensure this is correct
+    tasks: data.tasks || [],
   };
+
   return <SumaryTask amout={amout} />;
 }
